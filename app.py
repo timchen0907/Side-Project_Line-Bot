08f9a449f5b4474f3dcd3dc7ee9c4bde3c8142c3
陳政廷(Tim) <75665068+timchen0907@users.noreply.github.com>
2023-06-06 17:10:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -109,8 +109,31 @@ def message_text(event):
                 event.reply_token, TextSendMessage(text='抱歉，你只能繼續跟我1v1'))
     elif 'meme' in event.message.text:
         try: 
-            image_link = meme_programmer()
-            line_bot_api.reply_message(event.reply_token, ImageSendMessage(image_link, image_link))
+            url = "https://programming-memes-images.p.rapidapi.com/v1/memes"
+    
+            headers = {
+                "X-RapidAPI-Key": "2eb4ae18demsha492b3b31ae7229p11a89ajsn374987e29bb9",
+                "X-RapidAPI-Host": "programming-memes-images.p.rapidapi.com"
+            }
+            response = requests.get(url, headers=headers)
+
+            api_return = response.json()
+
+            # res_img = requests.get(api_return[0]['image'])
+            # image = res_img.content
+            # from PIL import Image
+            # import io
+            # stream = io.BytesIO(image)    
+            # image2 = Image.open(stream)
+            client_id = '911cca3455d90f1'
+            client_secret = '6abd7b8943b34f18ba0500836812171a0a687725'
+            access_token = '2e57b37c2f63e005904008e26fa3374a4af92ac3'
+            refresh_token = '10808e6a470f8b172a8188d151d11c6a343d2242'
+
+            client = imgurpython.ImgurClient(client_id, client_secret, access_token, refresh_token)
+
+            response = client.upload_from_url(api_return[0]['image'], config = None, anon = True)
+            line_bot_api.reply_message(event.reply_token, ImageSendMessage(response['link'], response['link']))
         except:
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text= 'Sorry~故障囉！'))
     else:
